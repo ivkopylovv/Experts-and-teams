016887db232fc21348beb59007cec913b8f60740
qusavin <qusavin@list.ru>
2022-11-22 11:17:01 +0300

    feat: added auth config

--- src/main/java/ru/rsreu/expertsandteams/config/AuthConfig.java
+++ src/main/java/ru/rsreu/expertsandteams/config/AuthConfig.java
@@ -1,4 +1,29 @@
 package ru.rsreu.expertsandteams.config;
 
+import ru.rsreu.expertsandteams.enums.Role;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class AuthConfig {
+
+    private static final Map<Role, ArrayList<String>> mapConfig = new HashMap<>();
+
+    static {
+        init();
+    }
+
+    private static void init() {
+        // TODO
+    }
+
+    public static Set<Role> getAllAppRoles() {
+        return mapConfig.keySet();
+    }
+
+    public static ArrayList<String> getUrlPatternsForRole(Role role) {
+        return mapConfig.get(role);
+    }
 }
