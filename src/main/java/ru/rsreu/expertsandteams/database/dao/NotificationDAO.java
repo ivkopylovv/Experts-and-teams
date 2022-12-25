aba2acfe9c65f91dc62299ebeb4817c4ed5a9325
ivkopylovv <vanyakopylovv@mail.ru>
2022-12-25 16:57:22 +0300

    feat: add doc for notification

--- src/main/java/ru/rsreu/expertsandteams/database/dao/NotificationDAO.java
+++ src/main/java/ru/rsreu/expertsandteams/database/dao/NotificationDAO.java
@@ -4,10 +4,36 @@ import ru.rsreu.expertsandteams.model.entity.Notification;
 
 import java.util.List;
 
+/**
+ * The Data Access Object that providing access
+ * to Notification Entity.
+ *
+ * @author Ivan Kopylov
+ * @author Mikhail Proskuryakov
+ * @version 1.0
+ *
+ */
 public interface NotificationDAO {
+
+    /**
+     * Saves Notification Entity to DB
+     *
+     * @param notification required Notification Entity to save
+     */
     void save(Notification notification);
 
+    /**
+     * Deletes Notification Entity from DB by ID
+     *
+     * @param id ID of required Notification Entity
+     */
     void deleteById(Long id);
 
+    /**
+     * Finds for all Notification Entities
+     * of User with userID
+     * @param userId ID of requesting User
+     * @return list of found Notification objects
+     */
     List<Notification> findByUserId(Long userId);
 }
--- src/main/java/ru/rsreu/expertsandteams/model/entity/Notification.java
+++ src/main/java/ru/rsreu/expertsandteams/model/entity/Notification.java
@@ -2,45 +2,114 @@ package ru.rsreu.expertsandteams.model.entity;
 
 import java.util.Date;
 
+/**
+ * Class that reflects the representation of the
+ * Notification Entity in the DB in the ORM style
+ *
+ * @author Ivan Kopylov
+ * @author Mikhail Proskuryakov
+ * @version 1.0
+ */
 public class Notification {
+    /**
+     * Unique identifier
+     */
     private Long id;
+
+    /**
+     * The User for whom the notification was created
+     * (ManyToOne relationship to {@link User})
+     */
     private User user;
+
+    /**
+     * Notification content
+     */
     private String message;
+
+    /**
+     * Date of creation
+     */
     private Date notificationDate;
 
+    /**
+     * Constructor for class {@link Notification}
+     *
+     * @param id {@link Notification#id}
+     * @param user {@link Notification#user}
+     * @param message {@link Notification#message}
+     * @param notificationDate {@link Notification#notificationDate}
+     */
+    public Notification(Long id, User user, String message, Date notificationDate) {
+        this.id = id;
+        this.user = user;
+        this.message = message;
+        this.notificationDate = notificationDate;
+    }
+
+    /**
+     * Constructor for class {@link Notification}
+     *
+     * @param user {@link Notification#user}
+     * @param message {@link Notification#message}
+     */
     public Notification(User user, String message) {
         this.user = user;
         this.message = message;
     }
 
+    /**
+     * {@link Notification#id}
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * {@link Notification#id}
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * {@link Notification#user}
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * {@link Notification#user}
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * {@link Notification#message}
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * {@link Notification#message}
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * {@link Notification#notificationDate}
+     */
     public Date getNotificationDate() {
         return notificationDate;
     }
 
+    /**
+     * {@link Notification#notificationDate}
+     */
     public void setNotificationDate(Date notificationDate) {
         this.notificationDate = notificationDate;
     }
